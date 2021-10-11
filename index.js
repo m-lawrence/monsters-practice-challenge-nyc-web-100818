73bd62178c5038feb1f8ace6d19608b62d0bfaaa
Melissa Lawrence <mlawrence537@gmail.com>
2021-10-11 16:00:28 -0400

    list objectives and set up html

---
+++ index.js
@@ -0,0 +1,9 @@
+// fetch monsters
+// display first 50 monsters w details
+// New monster form above list of monsters
+// Button onClick posts new monster to db
+// Button at the end of list of monsters onClick displays the next 50 monsters
+
+fetch('http://localhost:3000/monsters')
+    .then(r => r.json())
+    .then(data => console.log(data))
