5bec7b8a72b20466a3559188a5746ee4a4647ae6
Melissa Lawrence <mlawrence537@gmail.com>
2021-10-11 16:46:38 -0400

    start renderOneMonster function

--- index.js
+++ index.js
@@ -4,6 +4,22 @@
 // Button onClick posts new monster to db
 // Button at the end of list of monsters onClick displays the next 50 monsters
 
-fetch('http://localhost:3000/monsters')
-    .then(r => r.json())
-    .then(data => console.log(data))
+const monsterContainer = document.querySelector('div#monster-container')
+
+function renderOneMonster(monsterObj) {
+    const monsterDiv = document.createElement('div')
+    monsterDiv.classList.add('monster-card')
+    monsterDiv.dataset.id = monsterObj.id 
+
+    monsterDiv.innerHTML = `Name: ${monsterObj.name} Age: ${monsterObj.age} Description: ${monsterObj.description}`
+
+    monsterContainer.append(monsterDiv)
+}
+
+function renderAllMonsters() {
+
+    fetch('http://localhost:3000/monsters')
+        .then(r => r.json())
+        .then(data => console.log(data))
+
+}
